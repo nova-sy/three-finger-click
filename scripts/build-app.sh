e9bacd540e5257b04c27f429592fac4d17aa618e
Neo <flamingskycc@gmail.com>
2026-06-10 22:22:14 +0800

    Add macOS app icon

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -11,6 +11,9 @@ CONTENTS_DIR="$APP_DIR/Contents"
 MACOS_DIR="$CONTENTS_DIR/MacOS"
 RESOURCES_DIR="$CONTENTS_DIR/Resources"
 UNIVERSAL_BINARY_PATH="$ROOT_DIR/dist/$APP_NAME-universal"
+ICON_SOURCE="$ROOT_DIR/Assets/AppIcon.png"
+ICONSET_DIR="$ROOT_DIR/dist/AppIcon.iconset"
+ICNS_PATH="$RESOURCES_DIR/AppIcon.icns"
 
 cd "$ROOT_DIR"
 
@@ -33,6 +36,23 @@ fi
 
 chmod +x "$MACOS_DIR/$APP_NAME"
 
+if [[ -f "$ICON_SOURCE" ]]; then
+    rm -rf "$ICONSET_DIR"
+    mkdir -p "$ICONSET_DIR"
+    sips -z 16 16 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_16x16.png" >/dev/null
+    sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_16x16@2x.png" >/dev/null
+    sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_32x32.png" >/dev/null
+    sips -z 64 64 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_32x32@2x.png" >/dev/null
+    sips -z 128 128 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_128x128.png" >/dev/null
+    sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_128x128@2x.png" >/dev/null
+    sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_256x256.png" >/dev/null
+    sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_256x256@2x.png" >/dev/null
+    sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_512x512.png" >/dev/null
+    sips -z 1024 1024 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_512x512@2x.png" >/dev/null
+    iconutil -c icns "$ICONSET_DIR" -o "$ICNS_PATH"
+    rm -rf "$ICONSET_DIR"
+fi
+
 cat > "$CONTENTS_DIR/Info.plist" <<PLIST
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -44,6 +64,8 @@ cat > "$CONTENTS_DIR/Info.plist" <<PLIST
     <string>local.threefingerclick.app</string>
     <key>CFBundleName</key>
     <string>ThreeFingerClick</string>
+    <key>CFBundleIconFile</key>
+    <string>AppIcon</string>
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
